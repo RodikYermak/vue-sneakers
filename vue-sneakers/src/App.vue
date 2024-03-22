716fc3db6eb5acbb8a83416e638e408d5e7b5b46
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-03-22 16:09:18 -0700

    header

--- vue-sneakers/src/App.vue
+++ vue-sneakers/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-console.log('hey')
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+  </div>
 </template>
 
 <style scoped></style>
